0d318826114179b8377c9653ed9f80a39061ad54
serrrgun <serrrgun@mail.ru>
2021-05-04 13:50:39 +0300

    deploy: deploy test-task

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.header__btn')
-const menu = document.querySelector('.main-nav')
-
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('header__btn--active')
-  menu.classList.toggle('main-nav--open')
-})
